46ed637f0452ac19a779622292be1f6f120966d1
Micha-Er <erkelmicha@googlemail.com>
2025-07-09 23:15:57 +0200

    Small Changes in the structure of notebooks + introduction of time dependent Problems.

--- PINNLearning/data.py
+++ PINNLearning/data.py
@@ -12,9 +12,63 @@ def simp_sol(inp):
 
 # Generate data points in a range as tf tensores
 def gen_data(start, end, num):
-    # generate the data pouits and reshape them into a row vector
+    # generate the data points and reshape them into a row vector
     nums = np.linspace(start, end, num).reshape(-1, 1)
     return convert_to_tensor(nums, dtype=float32)
+
+
+# Generate random 2D data points in a range as tf tensores
+def gen_rand_2D_data(start, end, num):
+    # Smallest possible float increment
+    eps = np.finfo(float).eps
+
+    # generate the data points and ensure that the lower
+    # end cannot be part of the random interval
+    nums1 = np.random.uniform(start + eps, end, num)
+    nums2 = np.random.uniform(start + eps, end, num)
+
+    # Combine into a single array
+    xt_train = np.column_stack((nums1, nums2))
+    return convert_to_tensor(xt_train, dtype=float32)
+
+
+# Convert a numpy array into a fitting tensor
+def conv_data(np_array):
+    # Test if the data is oriented in column first fashion
+    if np_array.shape[0] < np_array.shape[-1]:
+        print("WARNING: Watch out for the dimensions of the data vector.")
+        print(f"-> The dimensions are: {np_array.shape}")
+
+    return convert_to_tensor(np_array, dtype=float32)
+
+
+# Returns a 2D tensor of the boundary conditions combined with another
+# tensor marking the the second dimensions values.
+def set_2D_boundaries(x_bc, y_bc, vec_sec_dim):
+    ax_bound = x_bc[0]  # lower or upper bound
+    bx_bound = x_bc[1]  # other bound
+    ay_bound = y_bc[0]  # value at first bound
+    by_bound = y_bc[1]  # value at second bound
+
+    # extend the boundary positon tensors to match the length of the vec_sec
+    ax_extended = np.resize(ax_bound, (vec_sec_dim.shape[0], 1))
+    bx_extended = np.resize(bx_bound, (vec_sec_dim.shape[0], 1))
+
+    # join the arrays of the boundary positions with the vec_sec
+    ax = tf.concat([conv_data(ax_extended), vec_sec_dim], axis=1)
+    bx = tf.concat([conv_data(bx_extended), vec_sec_dim], axis=1)
+
+    # join the upper and lower bound to reform the position vector
+    x = tf.concat([ax, bx], axis=0)
+
+    # extend the boundary value tensors to match the length of the vec_sec
+    ay_extended = tf.constant(ay_bound, shape=(vec_sec_dim.shape[0], 1))
+    by_extended = tf.constant(by_bound, shape=(vec_sec_dim.shape[0], 1))
+
+    # join the values for the upper and lower positons
+    y = tf.concat([ay_extended, by_extended], axis=0)
+
+    return x, y
 
 
 # Convert a set of boundary values to tf tensors
@@ -38,7 +92,7 @@ def add_noise(arr, noise_level, end_values=True, bounds=None):
 
 
 # Simulate the ODE accross the given discretized range
-def simp_sim(disc_x, y_bc, threshold=1e-6, noise_level=0.02, alph=None):
+def simp_sim(disc_x, y_bc, threshold=1e-6, noise_level=0.02):
     # improves speed as numpy doesnt like Tf tensors
     if isinstance(disc_x, tf.Tensor):
         disc_x = disc_x.numpy().reshape(-1,)
@@ -50,15 +104,6 @@ def simp_sim(disc_x, y_bc, threshold=1e-6, noise_level=0.02, alph=None):
     # initialze the solution vector
     u = np.zeros(num_points)
 
-    # check if a varying diffusion constant was provided
-    # --> adjust the finite differences if necessary
-    if alph is not None:
-        def denom(i):
-            return (del_x**2/alph(i * del_x) + 2)
-    else:
-        def denom(i):
-            return (del_x**2 + 2)
-
     # enforce the boundary conditions in the solution
     # as the iteration below doesnt cover these points,
     # they dont need to be reset
@@ -69,7 +114,7 @@ def simp_sim(disc_x, y_bc, threshold=1e-6, noise_level=0.02, alph=None):
         u_cp = u.copy()
 
         for i in range(1, num_points - 1):
-            u[i] = (u_cp[i + 1] + u_cp[i - 1]) / denom(i)
+            u[i] = (u_cp[i + 1] + u_cp[i - 1]) / (del_x**2 + 2)
 
         # Check convergence, stop early if possible
         diff = np.max(np.abs(u - u_cp))
@@ -83,3 +128,89 @@ def simp_sim(disc_x, y_bc, threshold=1e-6, noise_level=0.02, alph=None):
     # --> as these values will not be directly entered into a model,
     # they dont need to be a TF tensor
     return u.reshape(-1, 1), u_noisy.reshape(-1, 1)
+
+
+# Simulate the ODE accross the given inhomogeneous discretized range
+def alph_sim(disc_x, y_bc, alph, threshold=1e-6):
+    # improves speed as numpy doesnt like Tf tensors
+    if isinstance(disc_x, tf.Tensor):
+        disc_x = disc_x.numpy().reshape(-1,)
+
+    # calculate the discretization step
+    del_x = disc_x[1] - disc_x[0]
+    num_points = len(disc_x)
+
+    # initialze the solution vector
+    u = np.zeros(num_points)
+
+    # enforce the boundary conditions in the solution
+    # as the iteration below doesnt cover these points,
+    # they dont need to be reset
+    u[0] = y_bc[0][0]
+    u[-1] = y_bc[1][0]
+
+    for _ in range(80 * num_points):  # scale for convergence
+        u_cp = u.copy()
+
+        for i in range(1, num_points - 1):
+            alph_r = alph((i + 0.5) * del_x)
+            alph_l = alph((i - 0.5) * del_x)
+
+            u[i] = (alph_r * u_cp[i + 1] + alph_l * u_cp[i - 1]) \
+                / (del_x**2 + alph_l + alph_r)
+
+        # Check convergence, stop early if possible
+        diff = np.max(np.abs(u - u_cp))
+        if diff < threshold:
+            break
+
+    # reshape into the same data format as the gen_data
+    # --> as these values will not be directly entered into a model,
+    # they dont need to be a TF tensor
+    return u.reshape(-1, 1)
+
+
+# Simulate the time influenced PDE accross the material
+def time_sim(disc_x, y_bc, init_sol, max_sim_time, threshold=1e-6):
+    # improves speed as numpy doesnt like Tf tensors
+    if isinstance(disc_x, tf.Tensor):
+        disc_x = disc_x.numpy().reshape(-1,)
+
+    # calculate the discretization step in spatial domain
+    del_x = disc_x[1] - disc_x[0]
+
+    # calculate the discretization step in temporal domain
+    del_t = (del_x**2)/2
+    # FYI: set to ensure r = alpha*del_t/del_x^2 <= 1/2 (with alpha=0.1)
+    time_steps = int(max_sim_time // del_t)
+    r = 0.1 * del_t / del_x**2
+
+    # initialze the solution vector with the initial solution
+    u = init_sol(disc_x)
+    # set up a history for the time steps of the solution
+    u_hist = [u.copy()]
+
+    # enforce the boundary conditions in the solution
+    # as the iteration below doesnt cover these points,
+    # they dont need to be reset
+    u[0] = y_bc[0][0]
+    u[-1] = y_bc[1][0]
+
+    for t in range(time_steps):
+        u_cp = u.copy()
+        u[1:-1] = r * (u_cp[2:] + u_cp[:-2] - 2 * u_cp[1:-1]) + u_cp[1:-1]
+        # FYI: This is an alternative and more performant approach to the
+        # for loop from the other simulation function
+        u_hist.append(u)
+
+        # Check convergence, stop early if possible
+        diff = np.max(np.abs(u - u_cp))
+        if diff < threshold:
+            # print simulation information
+            print(f"Converged at time t={t*del_t:.5f}s in {t} steps.")
+            break
+
+    # reshape into the same data format as the gen_data
+    # --> as these values will not be directly entered into a model,
+    # they dont need to be a TF tensor
+    return u.reshape(-1, 1), u_hist, (t*del_t, max_sim_time)
--- PINNLearning/training.py
+++ PINNLearning/training.py
@@ -1,4 +1,5 @@
-from tensorflow import GradientTape, reduce_mean, square
+from tensorflow import GradientTape, reduce_mean, square, constant, \
+    tile, shape, data
 from keras import optimizers
 
 # FYI: All implementations here are made to be flexible and support various
@@ -11,24 +12,25 @@ from keras import optimizers
 def oneD_loss(model, inp, x_bc, y_bc, alph=None):
     # get the "output" from the model as values of the target function
     # and calculate the derivates with respect to the input features
-    with GradientTape(persistent=True) as tape:
-        tape.watch(inp)
-        y_pred = model(inp)
+    with GradientTape() as tape2:
+        tape2.watch(inp)
+        with GradientTape() as tape1:
+            tape1.watch(inp)
+            y_pred = model(inp)
+        # FYI: The tape records all operations, allowing automatic
+        # differentiation. You may call gradient inside of a tape, but
+        # only if you need a higher order without having the capacity
+        # to do the nested tapes. This is not best-practice however.
 
-        # the tape records all operations -->
-        # only call gradient inside of the tape if you need a higher order
-        y_x = tape.gradient(y_pred, inp)
-    y_xx = tape.gradient(y_x, inp)
-    del tape
+        y_x = tape1.gradient(y_pred, inp)
+        if alph is not None:
+            # adjust the derivative for a inhomogenuous
+            # material distribution
+            y_x = alph(inp) * y_x
+    y_xx = tape2.gradient(y_x, inp)
 
-    if alph is not None:
-        # calculate the residual of the ode
-        # for inhomogenuous material distribution
-        residual = y_pred - alph(inp) * y_xx
-    else:
-        # calculate the residual of the ode
-        # for a homogenuous material
-        residual = y_pred - y_xx
+    # calculate the residual of the ode
+    residual = y_pred - y_xx
     # compute mean squares error of the residual
     loss_pde = reduce_mean(square(residual))
 
@@ -44,6 +46,48 @@ def oneD_loss(model, inp, x_bc, y_bc, alph=None):
     return loss_pde + loss_bc
 
 
+# Implementation of the simplified but time dependent PDE as the loss
+# based on the residual and the boundary/ initial conditions
+def time_loss(model, inp, x_bc, y_bc, init_sol):
+    # here the inp is split in an tensor in the form of [x, t]
+    with GradientTape() as tape2:
+        tape2.watch(inp)
+        with GradientTape() as tape1:
+            tape1.watch(inp)
+            y_pred = model(inp)
+
+        # calculate the gradients with respect to both input variables
+        grads = tape1.gradient(y_pred, inp)
+        # split the calculated gradients of x and t
+        y_x = grads[:, 0:1]
+        y_t = grads[:, 1:2]
+    # calculate the second derivative of x
+    y_xx = tape2.gradient(y_x, inp)
+    y_xx = y_xx[:, 0:1]
+
+    # calculate the residual of the pde
+    residual = y_t - 0.1 * y_xx
+    # compute mean squares error of the residual
+    loss_pde = reduce_mean(square(residual))
+
+    # predict the values at the boundarys - here x_bc is 2D
+    y_bc_pred = model(x_bc)
+    # calculate the mean squared error of the boundaries
+    loss_bc = reduce_mean(square(y_bc - y_bc_pred))
+
+    # create a mask with ones in the first column and zeros in the second
+    mask = constant([[1.0, 0.0]], dtype=inp.dtype)
+    mask = tile(mask, [shape(inp)[0], 1])
+    # predict the values for the initial solution at t=0
+    y_init_pred = model(inp * mask)
+    # calculate the inital solution at the x values of the input data and get
+    # the mean squared error of the initial solution
+    init_vals = init_sol(inp[:, 0:1])
+    loss_init = reduce_mean(square(init_vals - y_init_pred))
+
+    return loss_pde + loss_bc + loss_init
+
+
 # Define an learning rate schedule to improve convergence
 def learning_rate_schedule(init, steps, rate):
     return optimizers.schedules.ExponentialDecay(
@@ -53,10 +97,40 @@ def learning_rate_schedule(init, steps, rate):
     )
 
 
+# Split the x_train set into batches of the size batch_size and enable the
+# distribution of these batches across the CPU
+def data_batch(data_set, batch_size):
+    # create a tf dataset fromt the given tf tensor, shuffle and batch it
+    data_batched = data.Dataset.from_tensor_slices(data_set)
+    data_batched = data_batched.shuffle(buffer_size=1024)
+    data_batched = data_batched.batch(batch_size)
+    data_batched = data_batched.prefetch(data.AUTOTUNE)
+    return data_batched
+
+
+# Definition of the inidvidual steps in training a NN
+def train_step(model, x_train, x_bc, y_bc, loss_func, optimizer):
+    # calculate the forward pass
+    with GradientTape() as tape:
+        loss = loss_func(model, x_train, x_bc, y_bc)
+
+    # calculate the backwards pass
+    # first calculate the gradients
+    grads = tape.gradient(loss, model.trainable_variables)
+    # let optimizer adjust values
+    optimizer.apply_gradients(zip(grads, model.trainable_variables))
+
+    return loss
+
+
 # Implementing the training function
 def train(model, x_train, x_bc, y_bc, loss_func, lr_schedule=None,
-          limit=3500, threshold=1e-9, write=True):
+          limit=3500, threshold=1e-9, batch=None, write=True):
     loss_time = []
+
+    # enable batching of x_train
+    if batch is not None:
+        x_batched = data_batch(x_train, batch)
 
     # enable setting of a learning rate scheduler
     if lr_schedule is not None:
@@ -69,17 +143,15 @@ def train(model, x_train, x_bc, y_bc, loss_func, lr_schedule=None,
     last_loss = 1
     epoch = 1
     while abs(delta_loss) > threshold:
-        # calculate the forward pass
-        with GradientTape() as tape:
-            loss = loss_func(model, x_train, x_bc, y_bc)
+        # call the train_step depending if x_train is batched or not
+        if batch is not None:
+            loss = 0
+            for batch in x_batched:
+                loss += train_step(model, batch, x_bc, y_bc, loss_func, optimizer)
+        else:
+            loss = train_step(model, x_train, x_bc, y_bc, loss_func, optimizer)
 
-        # calculate the backwards pass
-        # first calculate the gradients
-        grads = tape.gradient(loss, model.trainable_variables)
-        # let optimizer adjust values
-        optimizer.apply_gradients(zip(grads, model.trainable_variables))
-
-        # update all the variables
+        # update all algorithm the variables
         delta_loss = last_loss - loss
         last_loss = loss
         epoch += 1
@@ -87,7 +159,7 @@ def train(model, x_train, x_bc, y_bc, loss_func, lr_schedule=None,
 
         # stop if it takes too long
         if epoch >= limit:
-            print("BROKEN OFF TRANING: Did not converge!")
+            print("BROKEN OFF TRANING: Did not converge in limit!")
             break
         # print current state
         elif epoch % 100 == 0 and write:
